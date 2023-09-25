0508a3d8be290f28cb231d966de4974525487668
Jack Jenkins <jackjenk45@gmail.com>
2023-09-24 19:33:41 -0700

    starter frontend template

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # fyrefly
 
 Project for survey automation
+
+## Setup:
+
+### Backend Dependencies:
+
+- Using Go fiber framework
+
+### Frontend
+
+- Nextjs: requires Node v18 or higher
---
+++ frontend/pages/_app.js
@@ -0,0 +1,8 @@
+/**
+ * Use this file to import global CSS
+ */
+import '../styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
