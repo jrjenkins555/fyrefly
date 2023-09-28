b74d8c8539ed52214da1e2e5cf19920e6a1ed961
Jack Jenkins <jackjenk45@gmail.com>
2023-09-27 21:35:55 -0700

    using shadcn components

---
+++ frontend/components/ui/input-file-button.tsx
@@ -0,0 +1,51 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+
+export default function InputFileButton() {
+  const [file, setFile] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+  };
+
+  const handleUpload = async () => {
+    if (!file) {
+      alert('Please select a file to upload.');
+      return;
+    }
+
+    setUploading(true);
+
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+      const response = await fetch('http://localhost:8080/api/v1/upload', {
+        method: 'POST',
+        body: formData,
+      });
+      if (response.ok) {
+        console.log("File uploaded")
+      } else {
+        alert('File upload failed.');
+      }
+    } catch (error) {
+      // Handle network error or other issues
+      alert('An error occurred during the upload.');
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  return (
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="file">File</Label>
+      <Input id="file" accept=".pdf, .doc, .docx" onChange={handleFileChange} type="file" />
+      <Button type="submit" onClick={handleUpload} disabled={uploading}>
+        {uploading ? 'Uploading...' : 'Upload File'}
+      </Button>
+    </div>
+  )
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import FileUploadButton from '../components/file-upload-button';
+import Layout from '../components/layout';
 import utilStyles from '../styles/utils.module.css'
+import InputFileButton from '../components/ui/input-file-button';
 
 export default function Home() {
   return (
     // simple Layout component
     <Layout>
-      <FileUploadButton></FileUploadButton>
+      <InputFileButton></InputFileButton>
     </Layout>
   );
 }
