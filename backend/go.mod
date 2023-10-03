6e4cb6a5851b978e3a6d9937213fbe684f51449b
Jack Jenkins <jackjenk45@gmail.com>
2023-10-02 23:12:25 -0700

    starter for openai service

--- backend/go.mod
+++ backend/go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/otiai10/openaigo v1.6.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.49.0 // indirect
--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
     "github.com/gofiber/fiber/v2"
 	"github.com/jrjenkins555/fyrefly/pkg/routes"
 	"github.com/jrjenkins555/fyrefly/pkg/middleware"
+	"github.com/jrjenkins555/fyrefly/app/service"
 )
 
 func main() {
@@ -15,6 +16,8 @@ func main() {
 
 	// setup public routes
 	routes.PublicRoutes(app)
+
+	service.CallOpenAI()
 
 	fmt.Println("running on port 8080...")
     app.Listen(":8080")
